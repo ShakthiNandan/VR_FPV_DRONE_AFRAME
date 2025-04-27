92bb6ffa1b6044106f7abd15d569d8aca03854bc
KAARUNYA44 <eunoia.kaar@gmail.com>
2025-04-27 10:57:50 +0530

    changes

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-# app.py
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Home page
 @app.route('/')
-def index():
-    return render_template('index.html')  # This will load our A-Frame VR page
+def home():
+    return render_template('index.html')
+
+# Training page
+@app.route('/training')
+def training():
+    return render_template('training.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
